7274de51f489d8cc7283749fed42da803f81ed0d
Ayoubioussoufa <Ayoubioussoufa@gmail.com>
2024-10-09 14:39:55 +0100

    Init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import KycForm from './components/KycForm';
+
+function App() {
+
+  return (
+    <>
+      <KycForm/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/KycForm.tsx
@@ -0,0 +1,13 @@
+function KycForm() {
+    <>
+        <form action="">
+            <input type="text" />Name
+            <input type="password" name="" id="" />Password
+            <input type="email" name="" id="" />Email
+            <input type="date" name="" id="" />Birthday
+            
+        </form>
+    </>
+}
+
+export default KycForm;
